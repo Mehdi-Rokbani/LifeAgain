7b3118cfcace77e748767623c38b452d7e05c22d
Mehdi-Rokbani <mehdirokbani4@gmail.com>
2025-11-05 17:54:04 +0100

    started working on the chat

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+import registerChatHandler from "./Socket/chatHandler.js";
+
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose  from 'mongoose';
 const app = express()
 app.use(express.json())
-
+dotenv.config();
 //affichage f console lel requests
-app.use(request, res, next => {
-    console.log(request.path, request.method)
+app.use((r, res, next) => {
+    console.log(r.path, r.method)
     next()
 })
+
+const httpServer = createServer(app);
+const io = new SocketIOServer(httpServer, {
+    cors: { origin: "*", credentials: true }
+});
+httpServer.listen(process.env.PORT || 4000, () =>
+    console.log("Server running on ", process.env.PORT || 4000)
+);
+
+registerChatHandler(io); // 👈 important
 
 //connection to db
 mongoose.connect(process.env.URL)
